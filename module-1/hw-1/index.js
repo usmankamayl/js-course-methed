ec71629907c51ed587732ea395526a15e56565c0
usmankamayl <usman.kamailaev@yandex.ru>
2021-12-19 20:43:22 +0300

    hw-1

---
+++ module-1/hw-1/index.js
@@ -0,0 +1,12 @@
+'use strict';
+// обязательное задание
+const product = 'Стол';
+const count = 12;
+const category = 'Офисняя мебель';
+const price = 8000;
+
+console.log(product);
+
+//необязательное задание
+
+console.log('Общая сумма товара: ', count * price);
