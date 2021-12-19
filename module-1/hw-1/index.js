44380c8069f9399d60fa6fa23f0bc9a3921bcc5e
usmankamayl <usman.kamailaev@yandex.ru>
2021-12-19 20:50:49 +0300

    hw-2

--- module-1/hw-1/index.js
+++ module-1/hw-1/index.js
@@ -2,7 +2,7 @@
 // обязательное задание
 const product = 'Стол';
 const count = 12;
-const category = 'Офисняя мебель';
+const category = 'Офисная мебель';
 const price = 8000;
 
 console.log(product);
@@ -10,3 +10,12 @@ console.log(product);
 //необязательное задание
 
 console.log('Общая сумма товара: ', count * price);
+
+{
+    const item = 'костюм';
+    const count = 10;
+    const category = 'одежда';
+    const price = 5400;
+
+    console.log(`общая сумма товара: ${count * price}`);
+}
